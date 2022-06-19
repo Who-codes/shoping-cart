0d1e2603458bb6a005fac8f7a9aaa364de434def
Sulav <sulavshrestha023@gmail.com>
2022-06-19 19:43:48 +0545

    cart structure

---
+++ src/CartContainer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import CartItem from "./CartItem";
+
+export default function CartContainer() {
+  return (
+    <main>
+      <div className="cart-container">
+        <CartItem />
+      </div>
+    </main>
+  );
+}
