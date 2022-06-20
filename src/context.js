01bffe7e30a2889c5a4dc27bc1b1ef6184e64c01
Sulav <sulavshrestha023@gmail.com>
2022-06-20 18:52:13 +0545

    fianl commit

--- src/context.js
+++ src/context.js
@@ -1,11 +1,10 @@
 import React, { useContext, useEffect, useReducer } from "react";
-import data from "./data";
 import reducer from "./reducer";
 
 const AppContext = React.createContext();
 
 const initialState = {
-  cart: data,
+  cart: [],
   loading: false,
   amount: 0,
   total: 0,
